640339f2acd3c7b53e73bfeb969da6c1ccf440b3
Komal <rastogikomal57@gmail.com>
2025-12-04 11:25:09 +0530

    mini project uploaded

--- Practice/classwork/control_statements.js
+++
@@ -1,3 +0,0 @@
-//conditional statements (if, if-else, if-else-if, switch)
-// looping statements( for, while, do while, for in, for of)
-
--- Practice/random_num/script.js
+++
@@ -1,6 +0,0 @@
-
-//Write a program to generate a random number between 1 and 100.
-let num = Math.random(1,100)
-console.log(num)
-
-//
